ede8be3e519ed32ba484ce4f04f82de8dfaa76e8
lyf <hflyf123@qq.com>
2018-05-12 23:52:37 +0800

    20180512

---
+++ nine/c1.py
@@ -0,0 +1,19 @@
+# 面向对象
+# 有意义的面向对象的代码
+# 类 = 面向对象
+# 类 对象
+
+
+class Student():
+    name = ''
+    age = 0
+
+    def print_file(self):
+        print('name:' + self.name)
+        print('age:'+str(self.age))
+
+# 行为和特征
+
+
+class StudentHomework():
+    homework_name = ''
