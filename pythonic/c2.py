17e0dca9566e37f4725de41b166233a559f9b22a
lyf <hflyf123@qq.com>
2018-06-07 16:27:28 +0800

    用字典代替switch语句

---
+++ pythonic/c2.py
@@ -0,0 +1,26 @@
+
+#字典代替switch语句
+def get_sunday():
+    return 'Sunday'
+
+
+def get_moday():
+    return 'Moday'
+
+
+def get_tuesday():
+    return 'Tuesday'
+
+def get_default():
+    return 'Unknown'
+
+switcher = {
+    0: get_sunday(),
+    1: get_moday(),
+    2: get_tuesday()
+}
+
+
+day = 1123
+day_name = switcher.get(day, get_default())
+print(day_name)
